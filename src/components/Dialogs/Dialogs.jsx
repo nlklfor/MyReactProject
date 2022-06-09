38898b25f299991b5b5b0617908407e024f4f853
Nikita <nik.slepcov1771@gmail.com>
2022-06-09 17:26:41 +0300

    method map added

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,21 +3,6 @@ import { NavLink } from "react-router-dom";
 import "./Dialogs.css"
 
 
-const SingleDialog = (props) => {
-    let path = "/dialogs/" + props.id;
-    return (
-        <div className="dialog__list-item">
-            <NavLink to={path} className="dialog__list-item_name" alt="">{props.name}</NavLink>
-        </div>
-    )
-}
-const Message = (props) => {
-    return (
-        <div className="messages">
-            <div className="messages__content">{props.message}</div>
-        </div>
-    )
-}
 let dialogsData = [
     { id: 1, name: "Dimych" },
     { id: 2, name: "Nikitos" },
@@ -33,22 +18,44 @@ let messagesData = [
     { id: 4, message: "Pon😐" }
 ]
 
+const SingleDialog = (props) => {
+    let path = "/dialogs/" + props.id;
+    return (
+        <div className="dialog__list-item">
+            <NavLink to={path} className="dialog__list-item_name" alt="">{props.name}</NavLink>
+        </div>
+    )
+}
+const Message = (props) => {
+    return (
+        <div className="messages">
+            <div className="messages__content">{props.message}</div>
+        </div>
+    )
+}
+
+
+let dialogsElements = dialogsData.map((dialog) => <SingleDialog name={dialog.name} id={dialog.id} />)
+let messagesElements = messagesData.map((everyMessage) => <Message message={everyMessage.message} />)
+
 const Dialogs = () => {
     return (
         <div className="dialogs">
             <div className="dialog__list">
-                <SingleDialog name={dialogsData[0].name} id={dialogsData[0].id} />
+                {dialogsElements}
+                {/* <SingleDialog name={dialogsData[0].name} id={dialogsData[0].id} />
                 <SingleDialog name={dialogsData[1].name} id={dialogsData[1].id} />
                 <SingleDialog name={dialogsData[2].name} id={dialogsData[2].id} />
                 <SingleDialog name={dialogsData[3].name} id={dialogsData[3].id} />
                 <SingleDialog name={dialogsData[4].name} id={dialogsData[4].id} />
-                <SingleDialog name={dialogsData[5].name} id={dialogsData[5].id} />
+                <SingleDialog name={dialogsData[5].name} id={dialogsData[5].id} /> */}
             </div>
             <div className="messages__list">
-                <Message message={messagesData[0].message} />
+                {messagesElements}
+                {/* <Message message={messagesData[0].message} />
                 <Message message={messagesData[1].message} />
                 <Message message={messagesData[2].message} />
-                <Message message={messagesData[3].message} />
+                <Message message={messagesData[3].message} /> */}
             </div>
         </div>
     )
--- src/components/MyPosts/MyPosts.jsx
+++ src/components/MyPosts/MyPosts.jsx
@@ -4,9 +4,10 @@ import "./MyPosts.css"
 
 let postsData = [
     { id: 1, message: "This team will never trade ⬇", image: 'https://www.si.com/.image/t_share/MTY4NDk3MTA3MjYyNzc3MTEw/joel-embiid.png' },
-    { id: 2, message: "Joel chmo", image: 'https://www.kindpng.com/picc/m/104-1042894_transparent-lebron-james-face-png-stephen-curry-face.png'  },
+    { id: 2, message: "Joel chmo", image: 'https://www.kindpng.com/picc/m/104-1042894_transparent-lebron-james-face-png-stephen-curry-face.png' },
     { id: 3, message: "NePon😐", image: 'https://www.si.com/.image/t_share/MTY4NDk3MTA3MjYyNzc3MTEw/joel-embiid.png' }
 ]
+let postsElements = postsData.map((post) => <Post image={post.image} message={post.message} />)
 
 
 const MyPosts = () => {
@@ -17,9 +18,10 @@ const MyPosts = () => {
             <textarea></textarea>
             <button className="posts__btn-add">Add post</button>
             <div className="posts_added">
-                <Post image={postsData[0].image} message={postsData[0].message} />
+                {postsElements}
+                {/* <Post image={postsData[0].image} message={postsData[0].message} />
                 <Post image={postsData[1].image} message={postsData[1].message} />
-                <Post image={postsData[2].image} message={postsData[2].message} />
+                <Post image={postsData[2].image} message={postsData[2].message} /> */}
             </div>
         </div>
     )
